47d5d2505c84632ed856221ba6bc21734349eed5
themosthigh1 <dickemmanuel@gmail.com>
2023-11-14 09:31:06 -0500

    Updated Links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,31 @@
-import './globals.css'
-import theme  from "./theme/theme";
+import "./globals.css";
+import theme from "./theme/theme";
 
-import { CssBaseline, ThemeProvider } from '@/components/GlobalWrapper';
-import NavBar from '@/components/NavBar';
-import Footer from '@/components/Footer';
+import { CssBaseline, ThemeProvider } from "@/components/GlobalWrapper";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "The Erotic Storyteller",
+  description: "Written exclusively by mister erotica, nIck tRammell",
+};
 
-
-export default function MainLayout({children,}: {children: React.ReactNode}) {
-
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <body>
-              <NavBar/>
-                {children}
-              <Footer/>
-          </body>
-        </ThemeProvider>
+        <CssBaseline />
+        <body>
+          <NavBar />
+          {children}
+          <Footer />
+        </body>
+      </ThemeProvider>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,37 @@
-import BookCard from '@/components/BookCard'
-import { Container, Grid } from '@/components/GlobalWrapper'
-import Content from '@/components/Home/Content'
-import { Box } from '@mui/material'
-import { getSeries, getBooks } from './lib/api/queries'
-import PageTitle from '@/components/PageTitle'
+import BookCard from "@/components/BookCard";
+import { Container, Grid } from "@/components/GlobalWrapper";
+import Content from "@/components/Home/Content";
+import { Box } from "@mui/material";
+import { getSeries, getBooks } from "./lib/api/queries";
+import PageTitle from "@/components/PageTitle";
 
 export default async function HomePage() {
+  const books = await getBooks();
+  const series = await getSeries();
 
-const books = await getBooks()
-const series = await getSeries()
-
-  
   return (
     <Box>
-      <Content/>
+      <Content />
       <Container>
         {series.length > 0 && (
           <PageTitle key={series[0]._id} title={series[0].name} />
         )}
         <Grid container spacing={2} mt={5} justifyContent="center">
-          {books.map((book)=> <BookCard key={book._id} id={book._id} price={book.price} slug={book.slug.current} description={book.description} image={book.image} title={book.title} urlForBuy={book.urlForBuy}/>)}
+          {books.map((book) => (
+            <BookCard
+              key={book._id}
+              id={book._id}
+              price={book.price}
+              slug={book.slug.current}
+              description={book.description}
+              image={book.image}
+              title={book.title}
+              urlForBuy={book.urlForBuy}
+            />
+          ))}
         </Grid>
-        <Grid container spacing={2} mt={5} justifyContent="center">
-        </Grid>
+        <Grid container spacing={2} mt={5} justifyContent="center"></Grid>
       </Container>
     </Box>
-  )
+  );
 }
