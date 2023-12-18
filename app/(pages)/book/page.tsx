bfb86345963a1eebcae04df0dfbbfaeff0c1aaf8
themosthigh1 <dickemmanuel@gmail.com>
2023-12-18 03:06:05 -0500

    updated book pages

--- app/(pages)/book/page.tsx
+++ app/(pages)/book/page.tsx
@@ -1,28 +1,44 @@
+import { client } from "@/sanity/lib/client";
+import Content from "./Content";
+import { Box, Container, Grid } from "@/components/GlobalWrapper";
+import { Book } from "@/app/models/book";
+import { Key } from "react";
+import PageTitle from "@/components/PageTitle";
 
-import { client } from "@/sanity/lib/client"
-import Content from "./Content"
-import { Box, Container, Grid } from "@/components/GlobalWrapper"
-import { Book } from "@/app/models/book"
-import { Key } from "react"
-import PageTitle from "@/components/PageTitle"
-
-
-const BookPage = async() => {
-  const query = `*[_type == "book"] | order(_createdAt asc){...}`
-  const books: Book= await client.fetch(query)
-  console.log(books)
+const BookPage = async () => {
+  const query = `*[_type == "book"] | order(_createdAt asc){...}`;
+  const books: Book = await client.fetch(query);
 
   return (
     <Box>
       <Container>
-        <PageTitle title={'Books'}/>
+        <PageTitle title={"Books"} />
       </Container>
       <Grid container spacing={2} mt={5} justifyContent="center">
-        {books.map((book: { _id: Key | null | undefined; price: number; slug: { current: any }; description: string; image: any; title: string; urlForBuy: string })=><Content key={book._id} price={book.price} description={book.description} image={book.image} title={book.title} slug={book.slug.current} urlForBuy={book.urlForBuy}/>)}
+        {books.map(
+          (book: {
+            _id: Key | null | undefined;
+            price: number;
+            slug: { current: any };
+            description: string;
+            image: any;
+            title: string;
+            urlForBuy: string;
+          }) => (
+            <Content
+              key={book._id}
+              price={book.price}
+              description={book.description}
+              image={book.image}
+              title={book.title}
+              slug={book.slug.current}
+              urlForBuy={book.urlForBuy}
+            />
+          )
+        )}
       </Grid>
     </Box>
+  );
+};
 
-  )
-}
-
-export default BookPage
+export default BookPage;
